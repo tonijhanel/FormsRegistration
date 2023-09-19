e94cd01f5a7fbbe957bce7390bd31145adb30018
Vijay Kumar J <jalagari@adobe.com>
2023-09-19 11:16:38 +0530

    removing adaptive form block

--- blocks/adaptiveform/libs/afb-builder.js
+++
@@ -1,5 +0,0 @@
-import * as defaultBuilder from './default-builder.js';
-import * as customerBuilder from '../customization/custom-builder.js';
-
-const result = { ...defaultBuilder, ...customerBuilder };
-export default result;
